048bc9516031c842f4637cae5a628a32c629173d
fifafif <fifav@seznam.cz>
2016-11-17 21:27:29 -0800

    Environment loader

---
+++ Assets/Scripts/Helpers/SetActiveOnAwake.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetActiveOnAwake : MonoBehaviour {
+
+    public bool isActive;
+
+    void Awake()
+    {
+        gameObject.SetActive(isActive);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Visual/Environments/EnvironmentLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnvironmentLoader : MonoBehaviour
+{
+    public string sceneName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        LoadEnvironment(sceneName);
+	}
+
+    private void LoadEnvironment(string sceneName)
+    {
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
